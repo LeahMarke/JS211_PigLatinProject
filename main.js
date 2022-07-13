bf71b1a5103fe963403122abf64aa941ad388687
leahmarke <lmarke@live.com>
2022-07-12 23:46:49 -0400

    works for word or sentence

--- main.js
+++ main.js
@@ -36,7 +36,6 @@ const pigLatin = (word) => {
   }
 };
 pigLatin("apple");
-
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
--- test.js
+++ test.js
@@ -1,28 +1,46 @@
 const pigLatin = (word) => {
   let cleanWord = word.trim().toLowerCase();
   let vowels = ["a", "e", "i", "o", "u"];
-  // let firstLetter = cleanWord.charAt(0);
   let chr = "";
   let i;
   for (i = 0; i < cleanWord.length; i++) {
     chr = cleanWord.charAt(i);
     if (chr == "a" || chr == "e" || chr == "i" || chr == "o" || chr == "u")
-      //  let indexFirstVowel = i;
       break;
   }
   if (i < cleanWord.length) {
-    console.log(`The first vowel is located at ${i}`);
+    // console.log(`The first vowel is located at ${i}`);
     let trimmedLetters = cleanWord.slice(0, i);
-    console.log(trimmedLetters);
+    // console.log(trimmedLetters);
     let trimmedWord = cleanWord.slice(i);
-    console.log(trimmedWord);
+    // console.log(trimmedWord);
     if (i === 0) {
-      console.log(`${trimmedWord}${trimmedLetters}yay`);
+      return `${trimmedWord}${trimmedLetters}yay`;
     } else {
-      console.log(`${trimmedWord}${trimmedLetters}ay`);
+      return `${trimmedWord}${trimmedLetters}ay`;
     }
   } else {
-    console.log(`No vowel found`);
+    return `No vowel found`;
   }
 };
-pigLatin("apple");
+
+const sentencePigLatin = (sentence) => {
+  // place all words into an Array , then capture array
+  let newArray = sentence.split(" ");
+  console.log(newArray);
+  // [I, am, happy, today]
+  // need to seperate each word out into it's own array
+  let seperateWord = newArray.map((word) => pigLatin(word));
+  console.log("Pig latin array:", seperateWord);
+  // loop through each word in the array and then each character like previously
+  // join back to a string
+  let newPigSentence = seperateWord.join(" ");
+  // let newPigSentence = seperateWord.slice(0);
+
+  console.log("hey there", newPigSentence);
+  // let newPigString = newPigSentence.join(" ");
+  // console.log(newPigString);
+  // return newPigString;
+  return newPigSentence;
+};
+sentencePigLatin("Hey there sweetie");
